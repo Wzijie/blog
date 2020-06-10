c9f7126a36dfeecea18b14a372422320a137eeb6
weizijie <weizijie@jifenzhi.group>
2020-06-10 18:45:48 +0800

    构建接口-因为执行完成后再返回数据会超时，故仅打印执行结果，接口立即返回开始构建信息

--- server/index.js
+++ server/index.js
@@ -11,9 +11,12 @@ app.use(express.json());
 app.post('/deploy', async (req, res) => {
   if (secret !== req.body.sender.login) return res.status(500).json({ error: 'invalid secret' });
 
+  // 不能等待执行完成再返回数据了，执行太久会超时，仅打印出结果日志
   deploy()
-    .then(() => res.json({ code: 200, message: 'deploy success' }))
-    .catch(err => res.status(500).json({ code: 500, message: err.message, error: err }))
+    .then(() => console.log({ code: 200, message: 'deploy success' }))
+    .catch(err => console.log({ code: 500, message: err.message, error: err }));
+
+  res.json({ code: 200, message: 'deploy start' });
 });
 
 app.get('/*', (req, res) => {
