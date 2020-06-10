f86a558f69de6f13599789d65cfade0db281d724
weizijie <weizijie@jifenzhi.group>
2020-06-10 18:51:12 +0800

    新增备案号

--- src/components/Layout/Footer/index.js
+++ src/components/Layout/Footer/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import styles from '../index.module.less';
 
-const Footer = () => <footer className={styles.footer}>备案信息预留</footer>;
+const Footer = () => (
+  <footer className={styles.footer}>
+    <a href="http://www.beian.miit.gov.cn" target="_blank" rel="noopener noreferrer">粤ICP备17006916号-1</a>
+  </footer>
+);
 
 export default Footer;
