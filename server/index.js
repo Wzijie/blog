4b2e36ffdcbb5c5a76ea050bfaec485301b7cecb
weizijie <weizijie@jifenzhi.group>
2020-06-11 17:33:53 +0800

    构建接口-将接口验证方式调整为校验签名

--- server/index.js
+++ server/index.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const path = require('path');
 const deploy = require('./deploy');
+const hmacSha1 = require('crypto-js/hmac-sha1');
 const secret = require('./secret');
 const app = express();
+
+const checkSignature = (message, githubSignature) => {
+  // 签名算法为 sha1 的 HMAC 16 进制值
+  const localSignature = `sha1=${hmacSha1(message, secret)}`;
+  if (localSignature !== githubSignature) return 'invalid secret';
+}
 
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.use(express.json());
 
 app.post('/deploy', async (req, res) => {
-  if (secret !== req.body.sender.login) return res.status(500).json({ error: 'invalid secret' });
+  // 校验签名，是否为GitHub的Webhook调用
+  const checkMessage = checkSignature(
+    JSON.stringify(req.body),
+    req.get('X-Hub-Signature'),
+  );
+  if (checkMessage) return res.status(500).json({ error: checkMessage });
 
   // 不能等待执行完成再返回数据了，执行太久会超时，仅打印出结果日志
   deploy()
