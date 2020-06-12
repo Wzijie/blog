a0e06b047c45b1581224cddad3091ac38c20969a
weizijie <weizijie@jifenzhi.group>
2020-06-12 17:06:21 +0800

    路由切换后滚动到页面顶部

--- src/views/App/index.js
+++ src/views/App/index.js
@@ -2,10 +2,12 @@ import React, { Suspense } from 'react';
 import { Switch, BrowserRouter as Router } from 'react-router-dom';
 import { getRouter } from '../../route';
 import Container from '../../components/Layout';
+import ScrollToTop from './ScrollToTop';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Container>
         <Suspense fallback={<div>Loading...</div>}>
           <Switch>
