6db9d050778a0502be2c4f7e790f97b62b5ac185
魏子杰 <981841273@qq.com>
2021-02-02 16:35:57 +0800

    根据路由调整网站标题

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import navConfig from '../../nav.config';
+import { routes } from '../../route';
 import styles from './index.module.less';
 import Sidebar from './Sidebar';
 import Footer from './Footer';
 import Header from './Header';
 
+const routePathInNameMap = routes.reduce((prev, { path, name }) => {
+  return { ...prev, [path]: name };
+}, {})
+
 const Container = ({ children }) => {
   const location = useLocation();
+
+  useEffect(() => {
+    document.title = `${routePathInNameMap[location.pathname]} - Wzijie Blog`;
+  }, [location]);
 
   return (
     <div className={styles.main}>
