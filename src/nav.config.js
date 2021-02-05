75f4c30bea023c6a523c8d7c301ab84a7cabff2b
魏子杰 <981841273@qq.com>
2021-02-05 16:26:44 +0800

    新增笔记文章-超过最大整数精度丢失

--- src/nav.config.js
+++ src/nav.config.js
@@ -3,33 +3,53 @@ const navConfig = [{
   children: [{
     name: '数组乱序',
     path: '/JS/array-disorder',
+    createTime: '2020-07-10 17:06',
+    updateTime: '2020-07-10 17:06',
   },{
     name: '用遍历来实现斐波那契数列',
     path: '/JS/fibonacci',
+    createTime: '2020-07-10 17:06',
+    updateTime: '2020-07-10 17:06',
   }, {
     name: 'img-zoom 图片宽高缩放',
     path: '/JS/img-zoom',
+    createTime: '2020-07-08 18:24',
+    updateTime: '2020-07-08 18:24',
   }],
 }, {
   name: 'CSS',
   children: [{
     name: 'backdrop-filter 实现毛玻璃效果',
     path: '/CSS/backdrop-filter',
+    createTime: '2020-06-10 16:09',
+    updateTime: '2020-06-10 16:09',
   }],
 }, {
   name: 'HTML',
   children: [],
 }, {
+  name: '笔记',
+  children: [{
+    name: '超过最大整数精度丢失',
+    path: '/note/超过最大整数精度丢失',
+    createTime: '2021-02-05 10:53',
+    updateTime: '2021-02-05 15:19',
+  }],
+}, {
   name: '其他',
   children: [{
     name: '基于GitHub Webhook自动部署',
     path: '/other/automatic-deploy',
+    createTime: '2020-06-12 15:05',
+    updateTime: '2020-06-13 15:14',
   }],
 }, {
   name: '随笔',
   children: [{
     name: '2019 年终总结',
     path: '/essay/summary-2019',
+    createTime: '2020-06-10 16:09',
+    updateTime: '2020-06-12 17:19',
   }],
 }];
 
