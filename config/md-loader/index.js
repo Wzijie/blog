3311448600b2c8187265e7748f587e5fb044a271
魏子杰 <981841273@qq.com>
2021-02-05 16:27:33 +0800

    优化md-loader中关于代码字符的替换逻辑

--- config/md-loader/index.js
+++ config/md-loader/index.js
@@ -2,22 +2,29 @@ const hljs = require('highlight.js');
 const fs = require('fs');
 const moment = require('moment');
 
-let fileIndex = 0;
-
 // 整片文章是用反引号``包裹的，所以如果文章内再出现``的话需要进行处理，比如代码块内
 // 具体就是将`abc${console.log(1)}`这样的文本转换成\`abc${'console.log(1)'}\`
 // ${}内的代码直接转成字符以及对反引号转义
+// const codeFormat = mdContent => {
+//   return mdContent.replace(/`.{0,}`/g, match => {
+//     const code = match.replace(/\$\{(.{0,})\}/g, (_, group) => `\${"${group}"}`);
+//     return `\\\`${code.slice(1, -1)}\\\``;
+//   });
+// }
+
+// 将代码字符中的反引号"`"以及${xxx}的"$"前面加上"\"字符进行转义
 const codeFormat = mdContent => {
-  return mdContent.replace(/`.{0,}`/g, match => {
-    const code = match.replace(/\$\{(.{0,})\}/g, (_, group) => `\${"${group}"}`);
-    return `\\\`${code.slice(1, -1)}\\\``;
-  });
+  // 将`xxxxx`替换为 \`xxxxx`\
+  const format1 = mdContent.replace(/`(.{0,})`/g, '\\`$1\\`');
+  // 将${xxxxx}替换为/${xxxxx}，2个$$变量表示插入一个"$"，$1为第一个捕获内容
+  const format2 = format1.replace(/\$(\{.{0,}\})/g, '\\$$$1');
+  return format2;
 }
 
 // 将要传递给babel-loader编译的React字符串模板写入到demo目录下，主要是调试用
-const createDemoSource = (source, fileName) => {
-  fileIndex++;
-  fs.writeFile(`demo/${fileName}${fileIndex}.js`, source, err => {
+const createDemoSource = (source, resourcePath) => {
+  const fileName = resourcePath.slice(resourcePath.lastIndexOf('\\'), resourcePath.lastIndexOf('.'));
+  fs.writeFile(`demo/${fileName}.js`, source, err => {
     if (err) throw err;
   });
 }
@@ -66,7 +73,7 @@ module.exports = async function (source) {
     export default () => (<div className="md-block" dangerouslySetInnerHTML={{__html: \`${codeFormat(content)}\`}} />);
   `;
 
-  process.env.MD_TEST && createDemoSource(result, 'test');
+  process.env.MD_TEST && createDemoSource(result, this.resourcePath);
 
   callback(null, result);
 }
